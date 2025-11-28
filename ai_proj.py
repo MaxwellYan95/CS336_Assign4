c405735b3e80e68f4bc9858fafd9c9e628a06b50
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-28 14:04:18 -0500

    split code into functions

--- ai_proj.py
+++ ai_proj.py
@@ -3,88 +3,79 @@ import pandas as pd
 from sqlalchemy import create_engine, inspect
 
 """ Formatting Connection String and Creating Engine """
+def getConnection() -> str:
+    # Your raw info
+    user = "postgres"
+    password = "CattieMcWorm" # The '@' and '/' would break a normal string
+    host = "localhost"
+    port = "5432"
+    dbname = "postgres"
 
-# Your raw info
-user = "postgres"
-password = "CattieMcWorm" # The '@' and '/' would break a normal string
-host = "localhost"
-port = "5432"
-dbname = "postgres"
+    # Encode the password (and user, just to be safe)
+    safe_password = urllib.parse.quote_plus(password)
+    safe_user = urllib.parse.quote_plus(user)
 
-# Encode the password (and user, just to be safe)
-safe_password = urllib.parse.quote_plus(password)
-safe_user = urllib.parse.quote_plus(user)
-
-# Construct the string
-connection_string = f"postgresql+psycopg2://{safe_user}:{safe_password}@{host}:{port}/{dbname}"
-print(connection_string) # Output will look like: postgresql+psycopg2://my_user:super%40secure%2Fpassword%21@localhost:5432/my_database
-
-# Create the connection engine
-engine = create_engine(connection_string)
+    # Construct the string
+    return f"postgresql+psycopg2://{safe_user}:{safe_password}@{host}:{port}/{dbname}"
 
 
 
 """ Extracting Schema """
+def getSchema(engine) -> str:
+    # Create an inspector
+    inspector = inspect(engine)
 
-# Create an inspector
-inspector = inspect(engine)
+    # Get Table Names
+    table_names = inspector.get_table_names()
 
-# Print Schema
-print(inspector.get_schema_names())
+    # Stores Schema
+    schema = ""
 
-# Get Table Names
-table_names = inspector.get_table_names()
-
-# Stores Schema
-schema = ""
-
-# Format Schema String
-for table in table_names:
-    schema += (str(table) + "(")
-    # Goes through columns
-    col_str = ""
-    for col in inspector.get_columns(table):
-        col_str += (str(col['name']) + ", ")
-    schema += (col_str[:len(col_str)-2] + "), ")
-schema = schema[:len(schema)-2]
-print(schema)
+    # Format Schema String
+    for table in table_names:
+        schema += (str(table) + "(")
+        # Goes through columns
+        col_str = ""
+        for col in inspector.get_columns(table):
+            col_str += (str(col['name']) + ", ")
+        schema += (col_str[:len(col_str)-2] + "), ")
+    return schema[:len(schema)-2]
 
 
 
 """ User Input """
-
-# Write your query
-query = "SELECT * FROM applicant_race LIMIT 100;"
-
-try:
-    # Extract data directly into a DataFrame
-    # Using 'with' ensures the connection closes automatically
-    with engine.connect() as connection:
-        df = pd.read_sql(query, connection)
-
-    # Display the table
-    print(df.head())
-
-except Exception as e:
-    print(f"Error connecting to Postgres: {e}")
-
-"""
-while True:
-    # 3. Write your query
-    query = "SELECT * FROM employees LIMIT 100;"
-
-    if query == "exit":
-        break
+def query(engine) -> str:
+    # Write your query
+    query = "SELECT * FROM applicant_race LIMIT 100;"
 
     try:
-        # 4. Extract data directly into a DataFrame
+        # Extract data directly into a DataFrame
         # Using 'with' ensures the connection closes automatically
         with engine.connect() as connection:
             df = pd.read_sql(query, connection)
 
-            # 5. Display the table
-            print(df.head())
-
+        # Display the table
+        return df.head()
     except Exception as e:
-        print(f"Error connecting to Postgres: {e}")
-"""
+        return f"Error connecting to Postgres: {e}"
+
+def main():
+    # Get connection string
+    connect = getConnection()
+    print(f"Connection String: {connect}")
+
+    # Create the connection engine
+    engine = create_engine(connect)
+
+    # Get schema name
+    schema = getSchema(engine)
+    print(f"Schema String: {schema}")
+
+    # Get query
+    queryStr = query(engine)
+    print(f"Query String\n: {queryStr}")
+
+
+
+if __name__ == "__main__":
+    main()
